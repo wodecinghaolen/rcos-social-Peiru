a588b4e048a622acef8a0c4f0fef1c197c502d1f
wodecinghaolen <yotsuba8629@outlook.com>
2021-06-25 11:39:25 -0400

    week 2 updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import { Admin, Resource } from 'react-admin'
-import restProvider from 'ra-data-simple-rest'
+import { Admin, Resource, fetchUtils } from 'react-admin'
+import simpleRestProvider from 'ra-data-simple-rest'
 import PostList from './components/PostList'
 import PostCreate from './components/PostCreate'
 import PostEdit from './components/PostEdit'
 import UserList from './components/UserList'
 import UserCreate from './components/UserCreate'
 import UserEdit from './components/UserEdit'
+import authProvider from './authProvider';
+import Dashboard from './Dashboard';
+import NotFound from './NotFound';
+
+const httpClient = (url, options = {}) => {
+  if (!options.headers) {
+      options.headers = new Headers({ Accept: 'application/json' });
+  }
+  const token = localStorage.getItem('token');
+  options.headers.set('Authorization', `Bearer ${token}`);
+  return fetchUtils.fetchJson(url, options);
+}
+const dataProvider = simpleRestProvider('http://localhost:3000', httpClient);
 
 function App() {
   return (
-    <Admin dataProvider={restProvider('http://localhost:3000')}>
+    <Admin catchAll={NotFound} dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
+      
+
       <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} />
       <Resource name='users' list={UserList} create={UserCreate} edit={UserEdit} />
     </Admin>
---
+++ client/src/authProvider.js
@@ -0,0 +1,51 @@
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK, AUTH_GET_PERMISSIONS } from 'react-admin';
+import decodeJwt from 'jwt-decode';
+
+export default (type, params) => {
+    // called when the user attempts to log in
+    if (type === AUTH_LOGIN) {
+        const { username, password } = params;
+        const request = new Request('https://mydomain.com/authenticate', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        })
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                const decodedToken = decodeJwt(token);
+                localStorage.setItem('token', token);
+                localStorage.setItem('role', decodedToken.role);
+            });
+    }
+    // called when the user clicks on the logout button
+    if (type === AUTH_LOGOUT) {
+        localStorage.removeItem('token');
+        return Promise.resolve();
+    }
+    // called when the API returns an error
+    if (type === AUTH_ERROR) {
+        const { status } = params;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('token');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    }
+    // called when the user navigates to a new location
+    if (type === AUTH_CHECK) {
+        return localStorage.getItem('token')
+            ? Promise.resolve()
+            : Promise.reject();
+    }
+    if (type === AUTH_GET_PERMISSIONS) {
+        const role = localStorage.getItem('role');
+        return role ? Promise.resolve(role) : Promise.reject();
+    }
+    return Promise.reject('Unknown method');
+};
